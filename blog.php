af50d48bee607ec6f47ad01af381c8fc2079d6ec
ReynanYap <reynanyap25@gmail.com>
2022-05-15 14:12:39 -0400

    initial files 9

---
+++ blog.php
@@ -0,0 +1,165 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Blog</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+
+			<div id="wrapper">
+				<header id="header" class="alt">
+                <a href="index.php" class="logo"><strong>BAKING</strong> <span>Website</span></a>
+					<nav>
+						<a href="#menu">Menu</a>
+					</nav>
+				</header>
+
+				<nav id="menu">
+				<ul class="links">
+                <li class="active"> <a href="index.php">Home </a> </li>
+                        <li> <a href="about us.php">About Us</a> </li>
+						<li> <a href="Recipe.php">Recipe</a> </li>
+		                <li> <a href="blog.php">Blog</a> </li>
+		                <li> <a href="author.php">Author</a> </li>
+		                <li><a href="contact.php">Contact Us</a></li>
+            		</ul>
+				</nav>
+
+					<div id="main" class="alt">
+							<section id="one">
+								<div class="inner">
+									<header class="major">
+                                        <?php
+										echo"<h1>Blog</h1>";
+                                        ?>
+									</header>
+								</div>
+							</section>
+
+							<section class="tiles">
+								<article>
+									<span class="image">
+                                    <img src="images/apple.jpg" alt="" />
+							</span>
+							<header class="major">
+								<?php
+								echo"<h3>Apple Pie</h3>"
+								?>
+								<p><br> <span>Reynan Yap</span> | <span>05/14/2022 10:30 </span> | <span>114</span></p>
+
+								<div class="major-actions">
+									<a href="read-blog.php" class="button small next scrolly">Read Blog</a>
+								</div>
+							</header>
+
+								</article>
+								<article>
+									<span class="image">
+                                    <img src="images/salad.jpg" alt="" />
+							</span>
+							<header class="major">
+								<?php
+								echo"<h3>Fruit Salad Cake</h3>"
+								?>
+								<p><br> <span>Reynan Yap</span> | <span>02/25/2022 9:30 </span> | <span>1k</span></p>
+
+								<div class="major-actions">
+									<a href="read-blog1.php" class="button small next scrolly">Read Blog</a>
+								</div>
+							</header>
+
+								</article>
+								<article>
+									<span class="image">
+										<img src="images/buko.jpg" alt="" />
+							</span>
+							<header class="major">
+								<?php
+								echo"<h3>Buko Pie</h3>"
+								?>
+								<p><br> <span>Reynan Yap</span> | <span>01/27/2022 1:30 </span> | <span>34k</span></p>
+
+								<div class="major-actions">
+									<a href="read-blog2.php" class="button small next scrolly">Read Blog</a>
+								</div>
+							</header>
+								</article>
+								<article>
+									<span class="image">
+										<img src="images/b6.jpg" alt="" />
+									</span>
+									<header class="major">
+									<?php
+								    echo"<h3>Brownie Cookies</h3>"
+								    ?>
+								    <p><br> <span>Reynan Yap</span> | <span>01/01/2022 4:30 </span> | <span>4k</span></p>
+
+								<div class="major-actions">
+									<a href="#" class="button small next scrolly">Read Blog</a>
+								</div>
+							</header>
+								</article>
+								<article>
+									<span class="image">
+										<img src="images/b4.jpg" alt="" />
+									</span>
+									<header class="major">
+									<?php
+								    echo"<h3>Hopia</h3>"
+								    ?>
+								    <p><br> <span>Reynan Yap</span> | <span>01/05/2022 8:00 </span> | <span>11k</span></p>
+
+								<div class="major-actions">
+									<a href="#" class="button small next scrolly">Read Blog</a>
+								</div>
+									</header>
+								</article>
+								<article>
+									<span class="image">
+										<img src="images/b7.jpg" alt="" />
+									</span>
+									<header class="major">
+									  <?php
+								      echo"<h3>Caramel Brownies</h3>"
+								      ?>
+								      <p><br> <span>Reynan Yap</span> | <span>01/14/2022 9:00 </span> | <span>5k</span></p>
+
+								<div class="major-actions">
+									<a href="#" class="button small next scrolly">Read Blog</a>
+								</div>
+									</header>
+								</article>
+							</section>
+
+					</div>
+
+					<footer id="footer">
+					<div class="inner">
+						<ul class="icons">
+							<li><a href="#" class="icon alt fa-twitter"><span class="label">Twitter</span></a></li>
+							<li><a href="#" class="icon alt fa-facebook"><span class="label">Facebook</span></a></li>
+							<li><a href="#" class="icon alt fa-instagram"><span class="label">Instagram</span></a></li>
+						</ul>
+						<ul class="copyright">
+							<li>Copyright © 2022 Reynan Yap</li>
+							<li> <a href="https://www.mscmarinduque.edu.ph/">Marinduque State College</a></li>
+						</ul>
+					</div>
+				</footer>
+			</div>
+
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
