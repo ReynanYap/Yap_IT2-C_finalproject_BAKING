66e485024d9632a1438d3cf550ce2e6ed19a37cf
ReynanYap <reynanyap25@gmail.com>
2022-05-15 14:18:09 -0400

    initial files 14

---
+++ cookies.php
@@ -0,0 +1,166 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Cookies</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+
+			<div id="wrapper">
+
+				<header id="header" class="alt">
+				<a href="index.php" class="logo"><strong>BAKING</strong> <span>Website</span></a>
+					<nav>
+						<a href="#menu">Menu</a>
+					</nav>
+				</header>
+
+				<nav id="menu">
+					<ul class="links">
+                    <li class="active"> <a href="index.php">Home </a> </li>
+                        <li> <a href="about us.php">About Us</a> </li>
+						<li> <a href="Recipe.php">Recipe</a> </li>
+		                <li> <a href="blog.php">Blog</a> </li>
+		                <li> <a href="author.php">Author</a> </li>
+		                <li><a href="contact.php">Contact Us</a></li>
+            		</ul>
+				</nav>
+
+					<div id="main" class="alt">
+							<section id="one">
+								<div class="inner">
+									<header class="major">
+                                        <?php
+										echo"<h1>Cookies</h1>";
+                                        ?>
+									</header>
+								</div>
+							</section>
+
+							<section class="tiles">
+								<article>
+									<span class="image">
+										<img src="images/b19.jpg" alt="" />
+									</span>
+									<header class="major">
+                                        <?php
+										echo"<h3>Vanilla Biscuits</h3>";
+										echo"<p>Take a break from the Christmas madness to make Christmas Cookies! These sugar cookies are classic vanilla biscuits.</p>";
+                                        ?>
+										<div class="major-actions">
+											<a href="#" class="button small next">Recipe</a>
+										</div>
+									</header>
+								</article>
+
+								<article>
+									<span class="image">
+										<img src="images/b2o.jpg" alt="" />
+									</span>
+									<header class="major">
+                                    <?php
+										echo"<h3>Ube Crinkle Cookies</h3>";
+										echo"<p>Ube crinkle cookies are so addictive: super soft, slightly chewy, and packed with the sweet and nutty flavor of ube.</p>";
+                                        ?>
+										<div class="major-actions">
+											<a href="#" class="button small next">Recipe</a>
+										</div>
+									</header>
+								</article>
+
+								<article>
+									<span class="image">
+										<img src="images/b20.jpg" alt="" />
+									</span>
+									<header class="major">
+                                    <?php
+										echo"<h3>Oreo Cookies</h3>";
+										echo"<p>These oreo cookies are loaded with 2 cups of chopped oreos! Crispy on the outside and chewy on the inside, this is an awesome oreo cookies recipe!</p>";
+                                        ?>
+										<div class="major-actions">
+											<a href="#" class="button small next">Recipe</a>
+										</div>
+									</header>
+								</article>
+
+								<article>
+									<span class="image">
+										<img src="images/b3.jpg" alt="" />
+									</span>
+									<header class="major">
+                                    <?php
+										echo"<h3>Mango Bar</h3>";
+										echo"<p>Mango Bars are great because they are deceivingly easy to make. A few simple tips and tricks will give you everything you need to bake your heart out and never look back.</p>";
+                                        ?>
+										<div class="major-actions">
+											<a href="#" class="button small next">Recipe</a>
+										</div>
+									</header>
+								</article>
+
+								<article>
+									<span class="image">
+										<img src="images/b4.jpg" alt="" />
+									</span>
+									<header class="major">
+                                    <?php
+										echo"<h3>Hopia</h3>";
+										echo"<p>Hopia is yet another favorite 'merienda' or snack for Filipinos.  Hopia is made of thin flaky pastry, traditionally filled with mung bean paste(Munggo).</p>";
+                                        ?>
+										<div class="major-actions">
+											<a href="#" class="button small next">Recipe</a>
+										</div>
+									</header>
+								</article>
+
+								<article>
+									<span class="image">
+										<img src="images/b21.jpg" alt="" />
+									</span>
+									<header class="major">
+                                    <?php
+										echo"<h3>Chocolate Crinkle Cookies</h3>";
+										echo"<p>Brownie-like chocolate crinkle cookies are coated in confectioners' sugar. They're so good!</p>";
+                                        ?>
+										<div class="major-actions">
+											<a href="#" class="button small next">Recipe</a>
+										</div>
+									</header>
+								</article>
+
+
+							</section>
+
+					</div>
+
+                    <footer id="footer">
+					<div class="inner">
+						<ul class="icons">
+							<li><a href="#" class="icon alt fa-twitter"><span class="label">Twitter</span></a></li>
+							<li><a href="#" class="icon alt fa-facebook"><span class="label">Facebook</span></a></li>
+							<li><a href="#" class="icon alt fa-instagram"><span class="label">Instagram</span></a></li>
+						</ul>
+						<ul class="copyright">
+							<li>Copyright © 2022 Reynan Yap</li>
+							<li> <a href="https://www.mscmarinduque.edu.ph/">Marinduque State College</a></li>
+						</ul>
+					</div>
+				</footer>
+
+			</div>
+
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
