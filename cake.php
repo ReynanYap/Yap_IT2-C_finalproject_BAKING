0c6576875448b723f2d1f8ea91204363197b9d23
ReynanYap <reynanyap25@gmail.com>
2022-05-15 14:20:16 -0400

    initial files 15

---
+++ cake.php
@@ -0,0 +1,166 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Cake</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+
+			<div id="wrapper">
+
+				<header id="header" class="alt">
+				<a href="index.php" class="logo"><strong>BAKING</strong> <span>Website</span></a>
+					<nav>
+						<a href="#menu">Menu</a>
+					</nav>
+				</header>
+
+				<nav id="menu">
+					<ul class="links">
+                    <li class="active"> <a href="index.php">Home </a> </li>
+                        <li> <a href="about us.php">About Us</a> </li>
+						<li> <a href="Recipe.php">Recipe</a> </li>
+		                <li> <a href="blog.php">Blog</a> </li>
+		                <li> <a href="author.php">Author</a> </li>
+		                <li><a href="contact.php">Contact Us</a></li>
+            		</ul>
+				</nav>
+
+					<div id="main" class="alt">
+							<section id="one">
+								<div class="inner">
+									<header class="major">
+                                        <?php
+										echo"<h1>Cake</h1>";
+                                        ?>
+									</header>
+								</div>
+							</section>
+
+							<section class="tiles">
+								<article>
+									<span class="image">
+										<img src="images/b16.jpg" alt="" />
+									</span>
+									<header class="major">
+                                        <?php
+										echo"<h3>Ube Cake</h3>";
+										echo"<p>A Filipino celebration isn’t complete without dessert, and one of the most famous cakes is ube cake from Red Ribbon or Goldilocks.</p>";
+                                        ?>
+										<div class="major-actions">
+											<a href="#" class="button small next">Recipe</a>
+										</div>
+									</header>
+								</article>
+
+								<article>
+									<span class="image">
+										<img src="images/b1.jpg" alt="" />
+									</span>
+									<header class="major">
+                                    <?php
+										echo"<h3>Buko Pie</h3>";
+										echo"<p>Buko pie is a Filipino type of pie made out of semi flaky pastry filled with custard made out of young coconut meat and condensed milk.</p>";
+                                        ?>
+										<div class="major-actions">
+											<a href="#" class="button small next">Recipe</a>
+										</div>
+									</header>
+								</article>
+
+								<article>
+									<span class="image">
+										<img src="images/b2.jpg" alt="" />
+									</span>
+									<header class="major">
+                                    <?php
+										echo"<h3>Capuccino Cake</h3>";
+										echo"<p>This coffee cake is pretty much every coffee lover’s dream come true, with a soft and moist coffee and walnut sponge,and an incredibly fluffy.</p>";
+                                        ?>
+										<div class="major-actions">
+											<a href="#" class="button small next">Recipe</a>
+										</div>
+									</header>
+								</article>
+
+								<article>
+									<span class="image">
+										<img src="images/b17.jpg" alt="" />
+									</span>
+									<header class="major">
+                                    <?php
+										echo"<h3>Chocolate Cake</h3>";
+										echo"<p>The Most Amazing Chocolate Cake is full of moist, chocolatey perfection. This is the chocolate cake you’ve been dreaming of!</p>";
+                                        ?>
+										<div class="major-actions">
+											<a href="#" class="button small next">Recipe</a>
+										</div>
+									</header>
+								</article>
+
+								<article>
+									<span class="image">
+										<img src="images/b18.jpeg" alt="" />
+									</span>
+									<header class="major">
+                                    <?php
+										echo"<h3>Banana Cake</h3>";
+										echo"<p>A cross between banana bread and a drizzle cake, this easy banana loaf recipe is a quick bake that can be frozen.</p>";
+                                        ?>
+										<div class="major-actions">
+											<a href="#" class="button small next">Recipe</a>
+										</div>
+									</header>
+								</article>
+
+								<article>
+									<span class="image">
+										<img src="images/b5.jpg" alt="" />
+									</span>
+									<header class="major">
+                                    <?php
+										echo"<h3>Apple Pie</h3>";
+										echo"<p>Our homemade apple pie is sure to be the best apple pie recipe you've made to date.It has a flaky, buttery crust and a tender, lightly-spiced apple pie filling.</p>";
+                                        ?>
+										<div class="major-actions">
+											<a href="#" class="button small next">Recipe</a>
+										</div>
+									</header>
+								</article>
+
+
+							</section>
+
+					</div>
+
+                    <footer id="footer">
+					<div class="inner">
+						<ul class="icons">
+							<li><a href="#" class="icon alt fa-twitter"><span class="label">Twitter</span></a></li>
+							<li><a href="#" class="icon alt fa-facebook"><span class="label">Facebook</span></a></li>
+							<li><a href="#" class="icon alt fa-instagram"><span class="label">Instagram</span></a></li>
+						</ul>
+						<ul class="copyright">
+							<li>Copyright © 2022 Reynan Yap</li>
+							<li> <a href="https://www.mscmarinduque.edu.ph/">Marinduque State College</a></li>
+						</ul>
+					</div>
+				</footer>
+
+			</div>
+
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
