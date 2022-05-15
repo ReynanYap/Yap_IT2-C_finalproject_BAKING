2af8c076c5bbfd7e6c0fd2cf485359d7bad35373
ReynanYap <reynanyap25@gmail.com>
2022-05-15 14:17:24 -0400

    initial files 13

---
+++ cupcake.php
@@ -0,0 +1,166 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Cupcake</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+
+			<div id="wrapper">
+
+				<header id="header" class="alt">
+				<a href="index.php" class="logo"><strong>BAKING</strong> <span>Website</span></a>
+					<nav>
+						<a href="#menu">Menu</a>
+					</nav>
+				</header>
+
+				<nav id="menu">
+					<ul class="links">
+                    <li class="active"> <a href="index.php">Home </a> </li>
+                        <li> <a href="about us.php">About Us</a> </li>
+						<li> <a href="Recipe.php">Recipe</a> </li>
+		                <li> <a href="blog.php">Blog</a> </li>
+		                <li> <a href="author.php">Author</a> </li>
+		                <li><a href="contact.php">Contact Us</a></li>
+            		</ul>
+				</nav>
+
+					<div id="main" class="alt">
+							<section id="one">
+								<div class="inner">
+									<header class="major">
+                                        <?php
+										echo"<h1>CupCake</h1>";
+                                        ?>
+									</header>
+								</div>
+							</section>
+
+							<section class="tiles">
+								<article>
+									<span class="image">
+										<img src="images/b10.jpg" alt="" />
+									</span>
+									<header class="major">
+                                        <?php
+										echo"<h3>Banana Muffins</h3>";
+										echo"<p>These banana muffins are soft, buttery, and spiced with cinnamon. They’re wonderful plain, with chopped walnuts, or even chocolate chips.</p>";
+                                        ?>
+										<div class="major-actions">
+											<a href="#" class="button small next">Recipe</a>
+										</div>
+									</header>
+								</article>
+
+								<article>
+									<span class="image">
+										<img src="images/b11.jpg" alt="" />
+									</span>
+									<header class="major">
+                                    <?php
+										echo"<h3>Chocolate Cupcakes</h3>";
+										echo"<p> The cupcake is so moist, it melts in your mouth and the frosting is made with real melted chocolate for a silky smooth, super chocolatey frosting.</p>";
+                                        ?>
+										<div class="major-actions">
+											<a href="#" class="button small next">Recipe</a>
+										</div>
+									</header>
+								</article>
+
+								<article>
+									<span class="image">
+										<img src="images/b12.jpg" alt="" />
+									</span>
+									<header class="major">
+                                    <?php
+										echo"<h3>Strawberry Cupcakes</h3>";
+										echo"<p>Strawberry Cupcakes are moist, tender and totally addicting! There are pieces of chopped strawberry in the cupcake and they are topped with strawberry buttercream!.</p>";
+                                        ?>
+										<div class="major-actions">
+											<a href="#" class="button small next">Recipe</a>
+										</div>
+									</header>
+								</article>
+
+								<article>
+									<span class="image">
+										<img src="images/b13.jpg" alt="" />
+									</span>
+									<header class="major">
+                                    <?php
+										echo"<h3>Oreo Cupcake</h3>";
+										echo"<p>Oreo cupcakes will melt in your mouth and that cookies and cream frosting on top is nearly too good too be true. </p>";
+                                        ?>
+										<div class="major-actions">
+											<a href="#" class="button small next">Recipe</a>
+										</div>
+									</header>
+								</article>
+
+								<article>
+									<span class="image">
+										<img src="images/b14.jpg" alt="" />
+									</span>
+									<header class="major">
+                                    <?php
+										echo"<h3>Vanilla Cupcake</h3>";
+										echo"<p>This is the only vanilla cupcake recipe you will need. They are super easy to make and the texture is light, fluffy, and melt-in-your-mouth good.</p>";
+                                        ?>
+										<div class="major-actions">
+											<a href="#" class="button small next">Recipe</a>
+										</div>
+									</header>
+								</article>
+
+								<article>
+									<span class="image">
+										<img src="images/b15.png" alt="" />
+									</span>
+									<header class="major">
+                                    <?php
+										echo"<h3>Corn Cupcakes</h3>";
+										echo"<p>Love corn? Don’t stop at dinner! These cupcakes look just like roasted corn and We're not sure this is what was meant by sweet corn.</p>";
+                                        ?>
+										<div class="major-actions">
+											<a href="#" class="button small next">Recipe</a>
+										</div>
+									</header>
+								</article>
+
+
+							</section>
+
+					</div>
+
+                    <footer id="footer">
+					<div class="inner">
+						<ul class="icons">
+							<li><a href="#" class="icon alt fa-twitter"><span class="label">Twitter</span></a></li>
+							<li><a href="#" class="icon alt fa-facebook"><span class="label">Facebook</span></a></li>
+							<li><a href="#" class="icon alt fa-instagram"><span class="label">Instagram</span></a></li>
+						</ul>
+						<ul class="copyright">
+							<li>Copyright © 2022 Reynan Yap</li>
+							<li> <a href="https://www.mscmarinduque.edu.ph/">Marinduque State College</a></li>
+						</ul>
+					</div>
+				</footer>
+
+			</div>
+
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
