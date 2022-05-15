d4fba06b3754b75594fb6be20034e306627d3bd2
ReynanYap <reynanyap25@gmail.com>
2022-05-15 14:09:45 -0400

    initial files 6

---
+++ recipe1.php
@@ -0,0 +1,90 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Recipe</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+
+			<div id="wrapper">
+            <header id="header" class="alt">
+					<a href="index.php" class="logo"><strong>BAKING</strong> <span>Website</span></a>
+					<nav>
+						<a href="#menu">Menu</a>
+					</nav>
+				</header>
+
+				<nav id="menu">
+					<ul class="links">
+                    <li class="active"> <a href="index.php">Home </a> </li>
+                        <li> <a href="about us.php">About Us</a> </li>
+						<li> <a href="Recipe.php">Recipe</a> </li>
+		                <li> <a href="blog.php">Blog</a> </li>
+		                <li> <a href="author.php">Author</a> </li>
+		                <li><a href="contact.php">Contact Us</a></li>
+            		</ul>
+				</nav>
+
+					<div id="main" class="alt">
+
+							<section id="one">
+								<div class="inner">
+									<header class="major">
+                                        <?php
+										echo"<h1>Danish Pastries</h1>";
+                                        ?>
+									</header>
+                                    <?php
+									echo"<a><strong>INGREDIENTS</strong></a>";
+									echo"<p>2 cups unsalted butter, softened</p>";
+									echo"<p>⅔ cup all-purpose flour</p>";
+									echo"<p>2 ½ cups milk</p>";
+									echo"<p>½ cup white sugar</p>";
+									echo"<p>2 teaspoons salt</p>";
+									echo"<p>4 ½ teaspoons active dry yeast</p>";
+									echo"<p>8 cups all-purpose flour</p>";
+									echo"<p>2 eggs</p>";
+									echo"<p>1 teaspoon lemon extract</p>";
+									echo"<p>1 teaspoon almond extract</p>";
+									echo"<a><strong><br>Directions</strong></a>";
+									echo"<p>Step 1. In a medium bowl, cream together the butter and 2/3 cup of flour. Divide into 2 equal parts, and roll each half between 2 pieces of waxed paper into a 6 x12 inch sheet. Refrigerate.</p>";
+									echo"<p>Step 2. In a large bowl, mix together the dry yeast and 3 cups of the remaining flour. In a small saucepan over medium heat, combine the milk, sugar and salt. Heat to 115 degrees F (43 degrees C), or just warm, but not hot to the touch. Mix the warm milk mixture into the flour and yeast along with the eggs, and lemon and almond extracts. Stir for 3 minutes. Knead in the remaining flour 1/2 cup at a time until the dough is firm and pliable. Set aside to rest until double in size.</p>";
+									echo"<p>Step 3. Cut the dough in half, and roll each half out to a 14 inch square. Place one sheet of the cold butter onto each piece of dough, and fold the dough over it like the cover of a book. Seal edges by pressing with fingers. Roll each piece out to a 20x 12 inch rectangle, then fold into thirds by folding the long sides in over the center. Repeat rolling into a large rectangle, and folding into thirds. Wrap in plastic and refrigerate for at least 30 minutes.</p>";
+									echo"<p>Step 4. Remove from the refrigerator one at a time, and roll and fold each piece two more times. Return to the refrigerator to chill again before shaping. If the butter gets too warm, the dough will become difficult to manage.</p>";
+									echo"<p>Step 5. To make danishes, roll the dough out to 1/4 inch thickness. The dough can be cut into squares, with a filling placed in the center. Fold 2 of the corners over the center to form a filled diamond shape. Or, fold the piece in half, cut into 1 inch strips, stretch, twist and roll into a spiral. Place a dollop of preserves or other filling in the center. Place danishes on an ungreased baking sheet, and let rise until doubled. Preheat the oven to 450 degrees F (220 degrees C). Danishes can be brushed with egg white for a shiny finish.</p>";
+									echo"<p>Step 6. Bake for 8 to 10 minutes in the preheated oven, or until the bottoms are golden brown.</p>";
+								    ?>
+                                    </div>
+							</section>
+
+					</div>
+
+                    <footer id="footer">
+					<div class="inner">
+						<ul class="icons">
+							<li><a href="#" class="icon alt fa-twitter"><span class="label">Twitter</span></a></li>
+							<li><a href="#" class="icon alt fa-facebook"><span class="label">Facebook</span></a></li>
+							<li><a href="#" class="icon alt fa-instagram"><span class="label">Instagram</span></a></li>
+						</ul>
+						<ul class="copyright">
+							<li>Copyright © 2022 Reynan Yap</li>
+							<li> <a href="https://www.mscmarinduque.edu.ph/">Marinduque State College</a></li>
+						</ul>
+					</div>
+				</footer>
+
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
