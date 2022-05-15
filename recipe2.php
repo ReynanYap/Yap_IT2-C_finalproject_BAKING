0dcdf0087e383ce75ba3ff4292f3da03c3a011e3
ReynanYap <reynanyap25@gmail.com>
2022-05-15 14:10:34 -0400

    initial files 7

---
+++ recipe2.php
@@ -0,0 +1,83 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Recipe</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+
+			<div id="wrapper">
+            <header id="header" class="alt">
+					<a href="index.php" class="logo"><strong>BAKING</strong> <span>Website</span></a>
+					<nav>
+						<a href="#menu">Menu</a>
+					</nav>
+				</header>
+
+				<nav id="menu">
+					<ul class="links">
+                    <li class="active"> <a href="index.php">Home </a> </li>
+                        <li> <a href="about us.php">About Us</a> </li>
+						<li> <a href="Recipe.php">Recipe</a> </li>
+		                <li> <a href="blog.php">Blog</a> </li>
+		                <li> <a href="author.php">Author</a> </li>
+		                <li><a href="contact.php">Contact Us</a></li>
+            		</ul>
+				</nav>
+
+					<div id="main" class="alt">
+
+							<section id="one">
+								<div class="inner">
+									<header class="major">
+                                        <?php
+										echo"<h1>Buko Pie</h1>";
+                                        ?>
+									</header>
+                                    <?php
+									echo"<a><strong>INGREDIENTS</strong></a>";
+									echo"<p>4 eggs</p>";
+									echo"<p>2 (12 fluid ounce) cans evaporated milk</p>";
+									echo"<p>1 buko (fresh coconut), meat grated</p>";
+									echo"<p>1 (14 ounce) can sweetened condensed milk</p>";
+									echo"<p>¼ cup white sugar</p>";
+									echo"<p>1 pinch salt</p>";
+									echo"<a><strong><br>Directions</strong></a>";
+									echo"<p>Step 1. Preheat oven to 350 degrees F (175 degrees C).</p>";
+									echo"<p>Step 2. Beat eggs in a bowl; add evaporated milk, buko, sweetened condensed milk, white sugar, and salt. Mix until well blended; pour into a round 3-quart baking dish. Place baking dish in a large baking pan and fill pan with enough water to come halfway up the sides of the dish.</p>";
+									echo"<p>Step 3. Bake in the preheated oven until a toothpick inserted in the center comes out clean, about 1 hour.</p>";
+								    ?>
+                                    </div>
+							</section>
+
+					</div>
+
+                    <footer id="footer">
+					<div class="inner">
+						<ul class="icons">
+							<li><a href="#" class="icon alt fa-twitter"><span class="label">Twitter</span></a></li>
+							<li><a href="#" class="icon alt fa-facebook"><span class="label">Facebook</span></a></li>
+							<li><a href="#" class="icon alt fa-instagram"><span class="label">Instagram</span></a></li>
+						</ul>
+						<ul class="copyright">
+							<li>Copyright © 2022 Reynan Yap</li>
+							<li> <a href="https://www.mscmarinduque.edu.ph/">Marinduque State College</a></li>
+						</ul>
+					</div>
+				</footer>
+
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
