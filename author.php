69abfb8da05300495cbc890670668072f9536877
ReynanYap <reynanyap25@gmail.com>
2022-05-15 14:03:50 -0400

    initial files 3

---
+++ author.php
@@ -0,0 +1,75 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Author</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+
+			<div id="wrapper">
+            <header id="header" class="alt">
+					<a href="index.php" class="logo"><strong>BAKING</strong> <span>Website</span></a>
+					<nav>
+						<a href="#menu">Menu</a>
+					</nav>
+				</header>
+
+				<nav id="menu">
+					<ul class="links">
+                    <li class="active"> <a href="index.php">Home </a> </li>
+                        <li> <a href="about us.php">About Us</a> </li>
+						<li> <a href="Recipe.php">Recipe</a> </li>
+		                <li> <a href="blog.php">Blog</a> </li>
+		                <li> <a href="author.php">Author</a> </li>
+		                <li><a href="contact.php">Contact Us</a></li>
+            		</ul>
+				</nav>
+
+					<div id="main" class="alt">
+
+							<section id="one">
+								<div class="inner">
+									<header class="major">
+                                        <?php
+										echo"<h1>Author</h1>"
+                                        ?>
+									</header>
+									<span class="image main"><img src="images/b8.jpg" alt="" /></span>
+                                    <?php
+									echo"<p><strong>-Reynan Merjan Yap </strong></p>";
+									echo"<p>I am <strong>Reynan Yap </strong>, 21 years old from Masiga, Gasan, Marinduque and I am second year college, taking course of IT (Information Technology) at Marinduque State College.</p>";
+								    ?>
+                                    </div>
+							</section>
+
+					</div>
+
+                    <footer id="footer">
+					<div class="inner">
+						<ul class="icons">
+							<li><a href="#" class="icon alt fa-twitter"><span class="label">Twitter</span></a></li>
+							<li><a href="#" class="icon alt fa-facebook"><span class="label">Facebook</span></a></li>
+							<li><a href="#" class="icon alt fa-instagram"><span class="label">Instagram</span></a></li>
+						</ul>
+						<ul class="copyright">
+							<li>Copyright © 2022 Reynan Yap</li>
+							<li> <a href="https://www.mscmarinduque.edu.ph/">Marinduque State College</a></li>
+						</ul>
+					</div>
+				</footer>
+
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
