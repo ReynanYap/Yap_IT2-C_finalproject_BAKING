a75c924a17abaf6979df2f584b2f1e42ce83f014
ReynanYap <reynanyap25@gmail.com>
2022-05-15 14:11:15 -0400

    initial files 8

---
+++ recipe3.php
@@ -0,0 +1,86 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Recipe</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+
+			<div id="wrapper">
+            <header id="header" class="alt">
+					<a href="index.php" class="logo"><strong>BAKING</strong> <span>Website</span></a>
+					<nav>
+						<a href="#menu">Menu</a>
+					</nav>
+				</header>
+
+				<nav id="menu">
+					<ul class="links">
+                    <li class="active"> <a href="index.php">Home </a> </li>
+                        <li> <a href="about us.php">About Us</a> </li>
+						<li> <a href="Recipe.php">Recipe</a> </li>
+		                <li> <a href="blog.php">Blog</a> </li>
+		                <li> <a href="author.php">Author</a> </li>
+		                <li><a href="contact.php">Contact Us</a></li>
+            		</ul>
+				</nav>
+
+					<div id="main" class="alt">
+
+							<section id="one">
+								<div class="inner">
+									<header class="major">
+                                        <?php
+										echo"<h1>Cappuccino Cake</h1>";
+                                        ?>
+									</header>
+                                    <?php
+									echo"<a><strong>INGREDIENTS</strong></a>";
+									echo"<p>250g pack butter , softened</p>";
+									echo"<p>250g light soft brown sugar plus 2-3 tbsp</p>";
+									echo"<p>300g self-raising flour</p>";
+									echo"<p>4 eggs , beaten</p>";
+									echo"<p>50g walnuts , toasted and finely chopped (a food processor is easiest), optional</p>";
+									echo"<p>200ml very strong coffee (made fresh or with instant), cooled</p>";
+									echo"<p>500g tub mascarpone</p>";
+									echo"<p>2 tbsp light soft brown sugar</p>";
+									echo"<p>Cocoa powder or drinking chocolate to decorate</p>";
+									echo"<a><strong><br>Directions</strong></a>";
+									echo"<p>Step 1. Heat oven to 180C/fan 160C/gas 4. Butter 2 x 20cm sandwich tins and line the bottoms with greaseproof paper. Beat butter and sugar together with electric beaters until pale and creamy. Add the fl our and eggs in one go and keep beating until evenly mixed. Fold in the walnuts (if using) and half of the coffee. Spoon the mix into the prepared tins and bake for 25-30 mins or until golden and well risen.</p>";
+									echo"<p>Step 2. Leave the cakes in their tins for 5 mins before turning onto a wire rack. Sweeten the remaining coffee with the extra sugar and sprinkle 4 tbsp over the sponges. Leave to cool completely.</p>";
+									echo"<p>Step 3. While the cakes cool, make the frosting. Tip the mascarpone into a large bowl and beat in the sugar and remaining coffee until smooth and creamy. Use about half of the frosting to sandwich the sponges together then, using a palette or cutlery knife, spread the rest of the frosting over the top of the cake. Decorate with a dusting of cocoa powder or drinking chocolate. If you’re making this cake to eat at home, it will keep covered in the fridge for 2-3 days.</p>";
+								    ?>
+                                    </div>
+							</section>
+
+					</div>
+
+                    <footer id="footer">
+					<div class="inner">
+						<ul class="icons">
+							<li><a href="#" class="icon alt fa-twitter"><span class="label">Twitter</span></a></li>
+							<li><a href="#" class="icon alt fa-facebook"><span class="label">Facebook</span></a></li>
+							<li><a href="#" class="icon alt fa-instagram"><span class="label">Instagram</span></a></li>
+						</ul>
+						<ul class="copyright">
+							<li>Copyright © 2022 Reynan Yap</li>
+							<li> <a href="https://www.mscmarinduque.edu.ph/">Marinduque State College</a></li>
+						</ul>
+					</div>
+				</footer>
+
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
